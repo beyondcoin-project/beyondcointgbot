fc82b7172342d8ffd2c7963f404b09da37b98ff6
Tech1k <kristian@beyondcoin.io>
2020-12-31 10:19:49 -0700

    Add example config file

--- config.example.py
+++
@@ -1,9 +0,0 @@
-token = "BOT_API_TOKEN"
-coin = {
-    "ticker": "BYND",
-    "coin_name": "beyondcoin",
-    "minFee": "0.01",
-    "P2PKH_PREFIX": b'\x19',
-    "WIF_PREFIX": b'\xB0',
-}
-apiUrl = "https://api.beyondcoin.io"
